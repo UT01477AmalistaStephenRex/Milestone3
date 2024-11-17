358795b7f05f1b2e78dcb2bdc8a219949424d204
Amalista S. <UT01477@unicomtic.com>
2024-11-17 15:31:53 +0530

    initial

---
+++ Leave_Management_System/Leave_Management_System/Model/StudentLeaveRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Leave_Management_System.Model
+{
+    public class StudentLeaveRequest
+    {
+
+        [Key]
+        public int SLRId { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string Name { get; set; } = "nvarchar(100)";
+        [Column]
+        public string Reason { get; set; } = "nvarchar(500)";
+        [Column]
+        public string From { get; set; } = "nvarchar(10)";
+        [Column]
+        public string Days { get; set; } = "nvarchar(5)";
+        [Column]
+        public string AppliedOn { get; set; } = "nvarchar(10)";
+    }
+}
